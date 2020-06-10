cf63029d4983117cf573a7f57b1c62376b2a0766
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-10 20:26:44 +0300

    test IIFE calling axios and parsing HTML with cheerio

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const mongoose = require("mongoose");
 const { MONGO_URI } = require("./config");
 const axios = require("axios").default;
+const cheerio = require("cheerio");
+const cron = require("node-cron");
 
-const html = axios.get("https://cnnespanol.cnn.com/")
-  .then(data => {
-    console.log(data);
-  });
+(async () => {
+  const html = await axios.get("https://cnnespanol.cnn.com/");
+  const $ = cheerio.load(html.data);
+  const titles = $(".news__title");
+  
+  titles.each((index, element) => {
+    const breakingNews = {
+      title: $(element).text().toString(),
+      link: $(element).children().attr("href")
+    }
+  console.log(breakingNews);
+  })
+})();
 
 /*
 mongoose.connect(MONGO_URI, { 
