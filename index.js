de3ac75dc3c5885df7586a03dc4e1e327fedf8dd
FHHF <fabianhumbertohernandez@gmail.com>
2020-06-10 19:49:45 +0300

    GET fetching cnn español with axios

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const mongoose = require("mongoose");
 const { MONGO_URI } = require("./config");
+const axios = require("axios").default;
 
+const html = axios.get("https://cnnespanol.cnn.com/")
+  .then(data => {
+    console.log(data);
+  });
+
+/*
 mongoose.connect(MONGO_URI, { 
   useNewUrlParser: true, 
   useUnifiedTopology: true 
 });
-
-const Cat = mongoose.model("Cat", { name: String });
-
-const kitty = new Cat({ name: "Persick" });
-
-/*
-kitty
-  .save()
-  .then(cat => console.log(`Cat ${cat.name} has been saved`));  
 */
-Cat
-  .find()
-  .then(console.log);
